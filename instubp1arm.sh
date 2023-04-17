0e8bd1a60d1901a2dc70820de04a1f422c771492
Flavio Del Mestre <flavio@perunis.ch>
2023-04-17 19:26:42 +0200

    Update instubp1arm.sh

--- instubp1arm.sh
+++ instubp1arm.sh
@@ -14,8 +14,8 @@ hostnamectl set-hostname $2
 ##############################
 #### Change MariaDB repository
 ##############################
-apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-add-apt-repository 'deb [arch=ppc64el,amd64,arm64] https://mirror.mva-n.net/mariadb/repo/10.5/ubuntu focal main' 
+#apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+#add-apt-repository 'deb [arch=ppc64el,amd64,arm64] https://mirror.mva-n.net/mariadb/repo/10.5/ubuntu focal main' 
 
 apt update 
 #passwd root
